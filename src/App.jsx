0ed08e84c47a978bb8120c98d9851b0f55804c89
JoaoAfranio <joaoafraniorosa@hotmail.com>
2023-03-21 11:37:31 -0300

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import GlobalStyle from "./assets/css/GlobalStyle";
+import Home from "./pages/Home";
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Home />
+    </>
+  );
+}
