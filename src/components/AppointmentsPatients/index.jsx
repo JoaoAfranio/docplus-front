0c192e2801e8981ee371508200b1f2467e2001f0
JoaoAfranio <joaoafraniorosa@hotmail.com>
2023-04-03 20:54:16 -0300

    feat: adding api results

--- src/components/AppointmentsPatients/index.jsx
+++ src/components/AppointmentsPatients/index.jsx
@@ -1,35 +1,73 @@
 import { useState } from "react";
-import { BsCardList, BsFillPersonFill } from "react-icons/bs";
+import { BsCardList } from "react-icons/bs";
+import { FaBook } from "react-icons/fa";
 import { BiBlock } from "react-icons/bi";
 import { AiOutlineCalendar } from "react-icons/ai";
 import { HiOutlineThumbUp } from "react-icons/hi";
 import Info from "../GraphicPatients/Info";
 import Graph from "./Graph";
 import { Box, BoxGraph, Container, Content, Title } from "./style";
+import { useQuery } from "react-query";
+import appointmentApi from "../../services/appointmentApi";
 
 export default function AppointmentsPatients() {
   const [active, setActive] = useState("total");
+  const { data, isLoading, isError } = useQuery("graphic-appointments", appointmentApi.getGraphicAppointmentData);
 
-  const labels = ["January", "February", "March", "April", "May", "June", "July"];
-  const name = "Total de Pacientes";
-  const arrayData = [500, 600, 200, 350, 485, 950, 800];
+  const [labels, setLabels] = useState([]);
+  const [values, setValues] = useState([]);
+  const [name, setName] = useState("");
+
+  function setElementActive(e) {
+    setActive(e);
+    setName(e);
+    const { labels, values } = data[e];
+    setLabels(labels);
+    setValues(values);
+  }
+
+  const graphicName = name === "scheduleAppointments" ? "Consultas Agendadas" : name === "appointmentsConfirmed" ? "Média Mensal" : "Faltas";
 
   return (
     <Container>
       <Title>
-        <BsFillPersonFill /> Consultas
+        <FaBook /> Consultas
       </Title>
 
       <Content>
         <BoxGraph>
-          <Graph labels={labels} name={name} arrayData={arrayData} />
+          <Graph labels={labels} name={graphicName} arrayData={values} />
         </BoxGraph>
 
         <Box>
-          <Info icon={<HiOutlineThumbUp />} isActive={active === "total" ? true : false} description={"Realizadas"} data={"2565"} />
-          <Info icon={<AiOutlineCalendar />} isActive={active === "schedule" ? true : false} description={"Agendadas"} data={"168"} />
-          <Info icon={<BsCardList />} isActive={active === "average" ? true : false} description={"Média Mensal"} data={"106"} />
-          <Info icon={<BiBlock />} isActive={active === "absence" ? true : false} description={"Faltas"} data={"76"} />
+          <Info icon={<HiOutlineThumbUp />} description={"Realizadas"} data={data?.totalAppointments} />
+          <Info
+            setElementActive={() => {
+              setElementActive("scheduleAppointments");
+            }}
+            icon={<AiOutlineCalendar />}
+            isActive={active === "scheduleAppointments" ? true : false}
+            description={"Agendadas"}
+            data={data?.scheduleAppointments.values[0]}
+          />
+          <Info
+            setElementActive={() => {
+              setElementActive("appointmentsConfirmed");
+            }}
+            icon={<BsCardList />}
+            isActive={active === "appointmentsConfirmed" ? true : false}
+            description={"Média Mensal"}
+            data={data?.appointmentsConfirmed.values[0]}
+          />
+          <Info
+            setElementActive={() => {
+              setElementActive("appointmentsCanceled");
+            }}
+            icon={<BiBlock />}
+            isActive={active === "appointmentsCanceled" ? true : false}
+            description={"Faltas"}
+            data={data?.appointmentsCanceled.values[0]}
+          />
         </Box>
       </Content>
     </Container>
