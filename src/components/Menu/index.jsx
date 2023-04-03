10a3bfc72c73b44f94f087fdf180076c37bdbc1e
JoaoAfranio <joaoafraniorosa@hotmail.com>
2023-04-03 20:55:17 -0300

    refactor: removing class from items

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -9,19 +9,19 @@ export default function Menu() {
     <Container>
       <Title>docplus</Title>
       <Title className="mobile">D+</Title>
-      <Item activeClassName="active" to="/dashboard">
+      <Item to="/dashboard">
         <img src={Home} alt="home" /> <p>Home</p>
       </Item>
 
-      <Item activeClassName="active" to="/schedule">
+      <Item to="/schedule">
         <img src={Schedule} alt="schedule" /> <p>Agenda</p>
       </Item>
 
-      <Item activeClassName="active" to="/patient">
+      <Item to="/patient">
         <img src={Patient} alt="patient" /> <p>Pacientes</p>
       </Item>
 
-      <Item activeClassName="active" to="/profile">
+      <Item to="/profile">
         <img src={User} alt="profile" />
         <p>Perfil</p>
       </Item>
